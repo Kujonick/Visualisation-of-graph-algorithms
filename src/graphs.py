ed02e34b118e9c39ae9744bab49866565741a984
Kujonick <przemek.r@opoczta.pl>
2023-04-03 18:46:47 +0200

    TODO, extend graphs

--- src/graphs.py
+++ src/graphs.py
@@ -3,6 +3,7 @@ class Node:
         self.x = x
         self.y = y
         self.idx = idx
+        self.edges = dict()
 
     def __eq__(self, other) -> bool:
         if not isinstance(other,Node):
@@ -15,5 +16,18 @@ class Node:
     def __str__(self) -> str:
         return f"P[{self.idx}]({self.x},{self.y}"
     
+    # adds to 
     def connect(self, other) -> None:
         pass
+
+class Edge:
+    def __init__(self, node1, node2, directed, maxflow = None, flow = 0, minflow = None) -> None:
+        self.origin = node1
+        self.end = node2
+        self.directed = directed
+        self.maxflow = maxflow
+        self.flow = flow
+        self.minflow = minflow
+
+    def change_flow(self, change) :
+        self.flow += change
